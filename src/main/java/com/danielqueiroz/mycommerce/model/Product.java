a8137120e2ef3f45c96ab126d2cdeb38b5fb1bac
queirozdaniel <danielqueirozak77@gmail.com>
2021-03-01 23:17:50 -0300

    Mapping primitive type collection

--- src/main/java/com/danielqueiroz/mycommerce/model/Product.java
+++ src/main/java/com/danielqueiroz/mycommerce/model/Product.java
@@ -40,5 +40,11 @@ public class Product {
 
 	@OneToOne(mappedBy = "product")
 	private Stock stock;
+
+	@ElementCollection
+	@CollectionTable(name = "product_tag",
+			joinColumns = @JoinColumn(name = "product_id"))
+	@Column(name = "tag")
+	private List<String> tags;
 	
 }
